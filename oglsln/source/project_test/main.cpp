d3ec81333538fab9653ea318fbb345436a47d4f9
alwaystiys <alwaystiys@163.com>
2016-07-17 22:37:01 +0800

    change framework code

---
+++ oglsln/source/project_test/main.cpp
@@ -0,0 +1,117 @@
+/*
+
+	Copyright 2011 Etay Meiri
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include <math.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+#include "ogldev_util.h"
+#include "ogldev_app.h"
+#include "shader_technique.h"
+#include "ogldev_glut_backend.h"
+
+#define WINDOW_WIDTH  960
+#define WINDOW_HEIGHT 540
+#define WINDOW_TITLE "Test For OpenGL"
+
+class TemplateMain : public ICallbacks, public OgldevApp
+{
+public:
+
+    TemplateMain()
+    {
+       m_pEffect = NULL;
+    }
+
+    ~TemplateMain()
+    {
+		delete m_pEffect;
+    }
+
+    bool Init()
+    {
+		CreateVertexBuffer();
+		m_pEffect = new ShaderTechnique();	
+		m_pEffect->Enable();
+		if (!m_pEffect->Init()){
+			printf("Error initializing the lighting technique\n");
+            return false;	
+		}
+		m_pEffect->Enable();
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+        return true;
+    }
+
+    void Run()
+    {
+        GLUTBackendRun(this);
+    }
+
+    virtual void RenderSceneCB()
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+		glBindVertexArray(m_VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		GLUTBackendSwapBuffers();
+    }
+
+private:
+    void CreateVertexBuffer()
+    {
+        glGenVertexArrays(1, &m_VAO);
+		glBindVertexArray(m_VAO);
+
+		Vector3f vertivces[3];
+		vertivces[0] = Vector3f(-1.0f, -1.0f, 0.0f);
+		vertivces[1] = Vector3f( 0.0f, 1.0f, 0.0f);
+		vertivces[2] = Vector3f( 1.0f, -1.0f, 0.0f);
+		
+		glGenBuffers(1, &m_VBO);
+		glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertivces), vertivces, GL_STATIC_DRAW);
+
+		glDeleteVertexArrays(1, &m_VAO);
+    }
+	GLuint m_VAO;
+    GLuint m_VBO;
+	ShaderTechnique* m_pEffect;
+};
+
+
+int main(int argc, char** argv)
+{
+    GLUTBackendInit(argc, argv, false, false);
+
+	if (!GLUTBackendCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, false, WINDOW_TITLE)) {
+        return 1;
+    }
+
+    TemplateMain* pApp = new TemplateMain();
+
+    if (!pApp->Init()) {
+        return 1;
+    }
+
+    pApp->Run();
+
+    delete pApp;
+ 
+    return 0;
+}
--- oglsln/source/template_shader_version/main.cpp
+++ oglsln/source/template_shader_version/main.cpp
@@ -1,6 +1,6 @@
 /*
 
-	Copyright 2010 Etay Meiri
+	Copyright 2011 Etay Meiri
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -15,152 +15,103 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    Tutorial 04 - shaders
 */
 
-#include <stdio.h>
-#include <string.h>
+#include <math.h>
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 
 #include "ogldev_util.h"
-#include "ogldev_math_3d.h"
+#include "ogldev_app.h"
+#include "shader_technique.h"
+#include "ogldev_glut_backend.h"
 
-GLuint VBO;
+#define WINDOW_WIDTH  960
+#define WINDOW_HEIGHT 540
+#define WINDOW_TITLE "Test For OpenGL"
 
-const char* pVSFileName = "shader.vs";
-const char* pFSFileName = "shader.fs";
-
-static void RenderSceneCB()
+class TemplateMain : public ICallbacks, public OgldevApp
 {
-    glClear(GL_COLOR_BUFFER_BIT);
+public:
 
-    glEnableVertexAttribArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-
-    glDisableVertexAttribArray(0);
-
-    glutSwapBuffers();
-}
-
-
-static void InitializeGlutCallbacks()
-{
-    glutDisplayFunc(RenderSceneCB);
-}
-
-static void CreateVertexBuffer()
-{
-    Vector3f Vertices[3];
-    Vertices[0] = Vector3f(-1.0f, -1.0f, 0.0f);
-    Vertices[1] = Vector3f(1.0f, -1.0f, 0.0f);
-    Vertices[2] = Vector3f(0.0f, 1.0f, 0.0f);
-
- 	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
-}
-
-static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
-{
-    GLuint ShaderObj = glCreateShader(ShaderType);
-
-    if (ShaderObj == 0) {
-        fprintf(stderr, "Error creating shader type %d\n", ShaderType);
-        exit(0);
+    TemplateMain()
+    {
+       m_pEffect = NULL;
     }
 
-    const GLchar* p[1];
-    p[0] = pShaderText;
-    GLint Lengths[1];
-    Lengths[0]= strlen(pShaderText);
-    glShaderSource(ShaderObj, 1, p, Lengths);
-    glCompileShader(ShaderObj);
-    GLint success;
-    glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        GLchar InfoLog[1024];
-        glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
-        fprintf(stderr, "Error compiling shader type %d: '%s'\n", ShaderType, InfoLog);
-        exit(1);
+    ~TemplateMain()
+    {
+		delete m_pEffect;
     }
 
-    glAttachShader(ShaderProgram, ShaderObj);
-}
-
-static void CompileShaders()
-{
-    GLuint ShaderProgram = glCreateProgram();
-
-    if (ShaderProgram == 0) {
-        fprintf(stderr, "Error creating shader program\n");
-        exit(1);
-    }
-    
-    string vs, fs;
-
-    if (!ReadFile(pVSFileName, vs)) {
-        exit(1);
-    };
-
-    if (!ReadFile(pFSFileName, fs)) {
-        exit(1);
-    };
-
-    AddShader(ShaderProgram, vs.c_str(), GL_VERTEX_SHADER);
-    AddShader(ShaderProgram, fs.c_str(), GL_FRAGMENT_SHADER);
-
-    GLint Success = 0;
-    GLchar ErrorLog[1024] = { 0 };
-
-    glLinkProgram(ShaderProgram);
-    glGetProgramiv(ShaderProgram, GL_LINK_STATUS, &Success);
-	if (Success == 0) {
-		glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
-		fprintf(stderr, "Error linking shader program: '%s'\n", ErrorLog);
-        exit(1);
-	}
-
-    glValidateProgram(ShaderProgram);
-    glGetProgramiv(ShaderProgram, GL_VALIDATE_STATUS, &Success);
-    if (!Success) {
-        glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
-        fprintf(stderr, "Invalid shader program: '%s'\n", ErrorLog);
-        exit(1);
+    bool Init()
+    {
+		CreateVertexBuffer();
+		m_pEffect = new ShaderTechnique();	
+		m_pEffect->Enable();
+		if (!m_pEffect->Init()){
+			printf("Error initializing the lighting technique\n");
+            return false;	
+		}
+		m_pEffect->Enable();
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+        return true;
     }
 
-    glUseProgram(ShaderProgram);
-}
+    void Run()
+    {
+        GLUTBackendRun(this);
+    }
+
+    virtual void RenderSceneCB()
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+		glBindVertexArray(m_VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		GLUTBackendSwapBuffers();
+    }
+
+private:
+    void CreateVertexBuffer()
+    {
+        glGenVertexArrays(1, &m_VAO);
+		glBindVertexArray(m_VAO);
+
+		Vector3f vertivces[3];
+		vertivces[0] = Vector3f(-1.0f, -1.0f, 0.0f);
+		vertivces[1] = Vector3f( 0.0f, 1.0f, 0.0f);
+		vertivces[2] = Vector3f( 1.0f, -1.0f, 0.0f);
+		
+		glGenBuffers(1, &m_VBO);
+		glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertivces), vertivces, GL_STATIC_DRAW);
+
+		glDeleteVertexArrays(1, &m_VAO);
+    }
+	GLuint m_VAO;
+    GLuint m_VBO;
+	ShaderTechnique* m_pEffect;
+};
+
 
 int main(int argc, char** argv)
 {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB);
-    glutInitWindowSize(960, 540);
-    glutInitWindowPosition(100, 100);
-    glutCreateWindow("Tutorial 04");
+    GLUTBackendInit(argc, argv, false, false);
 
-    InitializeGlutCallbacks();
-
-    // Must be done after glut is initialized!
-    GLenum res = glewInit();
-    if (res != GLEW_OK) {
-      fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
-      return 1;
+	if (!GLUTBackendCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, false, WINDOW_TITLE)) {
+        return 1;
     }
-    
-    printf("GL version: %s\n", glGetString(GL_VERSION));
 
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    TemplateMain* pApp = new TemplateMain();
 
-    CreateVertexBuffer();
+    if (!pApp->Init()) {
+        return 1;
+    }
 
-    CompileShaders();
+    pApp->Run();
 
-    glutMainLoop();
-
+    delete pApp;
+ 
     return 0;
 }
