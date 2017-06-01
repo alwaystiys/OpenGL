bdfe91abfca9cc0ca63acce614084fc4861c0c4a
alwaystiys <alwaystiys@163.com>
2017-06-01 15:03:31 +0800

    replace lib and change code

--- ogl3.3/src/main.cpp
+++ ogl3.3/src/main.cpp
@@ -1,10 +1,10 @@
 #define HEAD_01 "Include/HelloTriangle.h"
-#define HEAD_02 "Include/HelloTriangle1.h" 
+#define HEAD_02 "Include/HelloTriangle1.h"
 #define HEAD_03 "Include/HelloTriangle2.h"
 
 #include HEAD_01
 
-int main(){
-	render();
-	return 0;
+int main() {
+    render();
+    return 0;
 }
