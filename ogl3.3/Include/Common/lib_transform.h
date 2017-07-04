8f569ef4037d4f9053fe85676be4670004ff480c
alwaystiys <alwaystiys@163.com>
2017-07-04 18:30:16 +0800

    add glm

---
+++ ogl3.3/Include/Common/lib_transform.h
@@ -0,0 +1,23 @@
+
+#ifndef LIB_TRANSFORM_H
+#define LIB_TRANSFORM_H
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+class Transform {
+
+public:
+
+private:
+
+
+
+};
+
+
+#endif LIB_TRANSFORM_H
+
+
+
